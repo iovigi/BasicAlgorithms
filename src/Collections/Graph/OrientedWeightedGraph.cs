3d6fadd1e7e0194110cf72b0241bd947eceb8b68
iovigi <il.nedelchev@gmail.com>
2016-11-05 14:39:16 +0200

    start graph

---
+++ src/Collections/Graph/OrientedWeightedGraph.cs
@@ -0,0 +1,255 @@
+﻿namespace Collection.Graph
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class OrientedWeightedGraph<T>
+    {
+        private readonly HashSet<Vertex> vertexes;
+
+        public OrientedWeightedGraph()
+        {
+            this.vertexes = new HashSet<Vertex>();
+        }
+
+        public bool Add(T value)
+        {
+            var vertex = new Vertex(value);
+
+            return this.vertexes.Add(vertex);
+        }
+
+        public bool Remove(T value)
+        {
+            var vertex = this.vertexes.FirstOrDefault(x => x.value.Equals(value));
+
+            if (vertex == null)
+            {
+                return false;
+            }
+
+            var result = this.vertexes.Remove(vertex);
+
+            if (result)
+            {
+                foreach (var candidateParent in this.vertexes.Where(v => v.edges.Any(e => e.toVertex == vertex)))
+                {
+                    candidateParent.edges.RemoveAll(x => x.toVertex == vertex);
+                }
+            }
+
+            return result;
+        }
+
+        public void AddEdge(T fromValue, T toValue, int cost)
+        {
+            var fromVertex = this.vertexes.FirstOrDefault(x => x.value.Equals(fromValue));
+            var toVertex = this.vertexes.FirstOrDefault(x => x.value.Equals(toValue));
+
+            if (fromVertex == null)
+            {
+                fromVertex = new Vertex(fromValue);
+                vertexes.Add(fromVertex);
+            }
+
+            if (toVertex == null)
+            {
+                toVertex = new Vertex(toValue);
+                vertexes.Add(toVertex);
+            }
+
+            fromVertex.edges.Add(new Edge(toVertex, cost));
+        }
+
+        public void RemoveEdge(T fromValue, T toValue, int cost)
+        {
+            var fromVertex = this.vertexes.FirstOrDefault(x => x.value.Equals(fromValue));
+
+            if (fromVertex == null)
+            {
+                fromVertex = new Vertex(fromValue);
+                vertexes.Add(fromVertex);
+            }
+
+            fromVertex.edges.RemoveAll(x => x.cost == cost && x.toVertex.value.Equals(toValue));
+        }
+
+        public void RemoveEdge(T fromValue, T toValue)
+        {
+            var fromVertex = this.vertexes.FirstOrDefault(x => x.value.Equals(fromValue));
+
+            if (fromVertex == null)
+            {
+                fromVertex = new Vertex(fromValue);
+                vertexes.Add(fromVertex);
+            }
+
+            fromVertex.edges.RemoveAll(x => x.toVertex.value.Equals(toValue));
+        }
+
+        public void Clear()
+        {
+            this.vertexes.Clear();
+        }
+
+
+        public IEnumerable<T> GetAllValues()
+        {
+            return this.vertexes.Select(x => x.value);
+        }
+
+        public IEnumerable<T> BFS(T startValue)
+        {
+            var startVertex = this.vertexes.FirstOrDefault(x => x.value.Equals(startValue));
+
+            if (startVertex == null)
+            {
+                yield break;
+            }
+
+            var queue = new Queue<Vertex>();
+            var used = new HashSet<Vertex>();
+
+            queue.Enqueue(startVertex);
+            used.Add(startVertex);
+
+            while(queue.Count > 0)
+            {
+                var currVertex = queue.Dequeue();
+
+                yield return currVertex.value;
+
+                foreach(var edge in currVertex.edges)
+                {
+                    if(used.Contains(edge.toVertex))
+                    {
+                        continue;
+                    }
+
+                    used.Add(edge.toVertex);
+                    queue.Enqueue(edge.toVertex);
+                }
+            }
+        }
+
+        public IEnumerable<T> DFS(T startValue)
+        {
+            var startVertex = this.vertexes.FirstOrDefault(x => x.value.Equals(startValue));
+
+            if(startVertex == null)
+            {
+                yield break;
+            }
+
+            var used = new HashSet<Vertex>();
+
+            foreach(var value in this.DFS(used,startVertex))
+            {
+                yield return value;
+            }
+        }
+
+        public bool HasCircle()
+        {
+            var used = new HashSet<Vertex>();
+
+            foreach (var vertex in this.vertexes)
+            {
+                if (used.Contains(vertex))
+                {
+                    continue;
+                }
+
+                if (HasCircle(used, vertex))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool HasCircle(HashSet<Vertex> used, Vertex currentVertex)
+        {
+            used.Add(currentVertex);
+
+            foreach (var edge in currentVertex.edges)
+            {
+                if (used.Contains(edge.toVertex))
+                {
+                    return true;
+                }
+
+                if (HasCircle(used, edge.toVertex))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private IEnumerable<T> DFS(HashSet<Vertex> used, Vertex currentVertex)
+        {
+            yield return currentVertex.value;
+
+            used.Add(currentVertex);
+
+            foreach (var edge in currentVertex.edges)
+            {
+                if (used.Contains(edge.toVertex))
+                {
+                    continue;
+                }
+
+                foreach (var value in this.DFS(used, edge.toVertex))
+                {
+                    yield return value;
+                }
+            }
+        }
+
+        private class Vertex
+        {
+            public readonly T value;
+
+            public readonly List<Edge> edges;
+
+            public Vertex(T value)
+            {
+                this.value = value;
+                this.edges = new List<Edge>();
+            }
+
+            public override int GetHashCode()
+            {
+                return this.value.GetHashCode();
+            }
+
+            public override bool Equals(object obj)
+            {
+                var vertex = obj as Vertex;
+
+                if (vertex == null)
+                {
+                    return false;
+                }
+
+                return this.value.Equals(vertex.value);
+            }
+        }
+
+        private class Edge
+        {
+            public readonly Vertex toVertex;
+            public readonly int cost;
+
+            public Edge(Vertex toVertex, int cost)
+            {
+                this.toVertex = toVertex;
+                this.cost = cost;
+            }
+        }
+    }
+}
